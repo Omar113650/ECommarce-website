7aba042b7b905d32ba5e4a0c1ed95e954525bfc0
Omar113650 <omarelhelaly520@gmail.com>
2025-11-13 18:09:46 +0200

    change capital number

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -81,7 +81,7 @@ export const getAllProducts = asyncHandler(async (req, res) => {
   // const skip = (Number(page) - 1) * Number(limit);
 
   const products = await Product.find(query)
-    .populate("categoryId", "Name")
+    .populate("categoryId", "name")
     .populate("Brand", "Name")
     .sort({ createdAt: -1 })
     // .skip(skip)
