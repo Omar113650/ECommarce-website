fbb5eb19fc1d8bcce68ef75499ddbc8d0258acf6
Omar113650 <omarelhelaly520@gmail.com>
2025-11-13 22:44:29 +0200

    add new version cart

--- routes/CartRoute.js
+++ routes/CartRoute.js
@@ -9,7 +9,7 @@ import { ValidatedID } from "../middlewares/validateID.js";
 import { OptionalVerifyToken } from "../middlewares/VerifyToken.js";
 const router = express.Router();
 
-router.post("/add-cart", OptionalVerifyToken, addToCart);
+router.post("/add-cart", addToCart);
 router.get("/get-cart", OptionalVerifyToken, getCart);
 router.delete("/remove/:id", OptionalVerifyToken, ValidatedID, removeFromCart);
 router.delete("/clear-Cart", OptionalVerifyToken, clearCart);
