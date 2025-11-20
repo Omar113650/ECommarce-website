ca88f31603cba80aa72b2889a674973d5ff0174f
Omar113650 <omarelhelaly520@gmail.com>
2025-11-20 12:37:58 +0200

    update validateId

--- middlewares/validateID.js
+++ middlewares/validateID.js
@@ -1,8 +1,26 @@
 import mongoose from "mongoose";
 
+// export const ValidatedID = (req, res, next) => {
+//   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+//     return res.status(400).json({ message: "Invalid ID" });
+//   }
+//   next();
+// };
+
+
+
+
+
+
+
 export const ValidatedID = (req, res, next) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+  const id = req.params.id;
+  
+  const isValid = /^[0-9a-fA-F]{24}$/.test(id);
+  
+  if (!isValid) {
     return res.status(400).json({ message: "Invalid ID" });
   }
+
   next();
 };
